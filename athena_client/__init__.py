925abcc4edc19a1a1bd5719cd6007a51730c66e9
Alvaro Alvarez <alvaro@example.com>
2026-01-02 17:01:15 -0500

    chore: bump version to 1.0.31 and update Makefile

--- athena_client/__init__.py
+++ athena_client/__init__.py
@@ -29,7 +29,7 @@ def get_sqlalchemy_connector() -> type:
         require_optional_package("sqlalchemy", "database operations", "db", err)
 
 
-__version__ = "1.0.30"
+__version__ = "1.0.31"
 
 __all__ = [
     "Athena",
